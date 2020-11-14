1d01b29f0c7a01ff912b8500e7eccf672ea5e623
dwnusa <dwnusa@gmail.com>
2020-11-14 22:10:25 +0900

    clean up all commented codes

--- dcm_editor/urls.py
+++ dcm_editor/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('index', views.index_as_view.as_view(), name='index_as_view'),
+    # path('', views.index, name='index'),
+    # path('index', views.index_as_view.as_view(), name='index_as_view'),
     path('splitter', views.Splitter.as_view(), name='splitter'),
 ]
--- dcm_editor/views.py
+++ dcm_editor/views.py
@@ -17,13 +17,13 @@ from io import BytesIO
 import time
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index!")
-
-
-class index_as_view(APIView):
-    def get(self, request, format=None):
-        return Response(data="index world!", status=status.HTTP_200_OK)
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index!")
+#
+#
+# class index(APIView):
+#     def get(self, request, format=None):
+#         return Response(data="index world!", status=status.HTTP_200_OK)
 
 
 class Splitter(APIView):
@@ -78,111 +78,3 @@ class Splitter(APIView):
         response['Content-Type'] = 'application/x-zip-compressed'
         response['Content-Disposition'] = 'attachment; filename=singleframes.zip'
         return response
-        # return Response(data="Welcome, Splitter post view", status=status.HTTP_200_OK)
-
-class Splitter_reconstruction(APIView):
-    # def get(self, format=None):
-    def get(self, request, format=None):
-        current_path = os.getcwd()
-        # file_name = "temp1.dcm"
-        # file_path = os.path.join(current_path, file_name)
-        # ds1 = pydicom.dcmread(file_path)
-        # buffer1 = io.BytesIO()
-        # # create a DicomFileLike object that has some properties of DataSet
-        # memory_dataset1 = DicomFileLike(buffer1)
-        # # write the dataset to the DicomFileLike object
-        # pydicom.dcmwrite(memory_dataset1, ds1)
-        # # to read from the object, you have to rewind it
-        # memory_dataset1.seek(0)
-        # # read the contents as bytes
-        # ds_bytes1 = memory_dataset1.read()
-        #
-        # buffer3 = io.BytesIO()
-        # zip_file = zipfile.ZipFile(buffer3, 'w')
-        # zip_file.writestr(file_name, ds_bytes1)
-        # zip_file.close()
-        # # Return zip
-        # response = HttpResponse(buffer3.getvalue())
-        # response['Content-Type'] = 'application/x-zip-compressed'
-        # response['Content-Disposition'] = 'attachment; filename=album.zip'
-        # return Response(data=response, status=status.HTTP_200_OK)
-        return Response(data="Hello world splitter!", status=status.HTTP_200_OK)
-
-    def post(self, request, format=None):
-        # extraction of information from request
-        # userId = request.user.id
-        myfiles = request.FILES.getlist('myfiles')
-        # Check if file attached
-        if not myfiles:
-            return Response(data="Files are not attached correctly", status=status.HTTP_400_BAD_REQUEST)
-        myfile = myfiles[0]
-        temp1 = copy.deepcopy(myfile)
-        temp2 = copy.deepcopy(myfile)
-        ds1 = pydicom.dcmread(temp1)
-        ds2 = pydicom.dcmread(temp2)
-        strMyfile = myfile.read()
-        # strMyfile = copy.deepcopy(myfile.read())
-        print(ds1)
-        pixel_array1, pixel_array2 = ds1.pixel_array[0, :, :], ds1.pixel_array[1, :, :]
-        # print(ds1)
-        ds1.NumberOfFrames = "1"
-        ds1.PixelData = pixel_array1
-        ds1.StudyInstanceUID = '1.2.124.113532.192.147.106.101.20080114.92117.7870431'
-
-        ds2.NumberOfFrames = "1"
-        ds2.PixelData = pixel_array2
-        ds2.StudyInstanceUID = '1.2.124.113532.192.147.106.101.20080114.92117.7870432'
-
-        buffer1 = io.BytesIO()
-        # create a DicomFileLike object that has some properties of DataSet
-        memory_dataset1 = DicomFileLike(buffer1)
-        # write the dataset to the DicomFileLike object
-        pydicom.dcmwrite(memory_dataset1, ds1)
-        # to read from the object, you have to rewind it
-        memory_dataset1.seek(0)
-        # read the contents as bytes
-        ds_bytes1 = memory_dataset1.read()
-
-        buffer2 = io.BytesIO()
-        # create a DicomFileLike object that has some properties of DataSet
-        memory_dataset2 = DicomFileLike(buffer2)
-        # write the dataset to the DicomFileLike object
-        pydicom.dcmwrite(memory_dataset2, ds2)
-        # to read from the object, you have to rewind it
-        memory_dataset2.seek(0)
-        # read the contents as bytes
-        ds_bytes2 = memory_dataset2.read()
-
-
-
-        filename1 = "temp1.dcm"
-        filename2 = "temp2.dcm"
-        # Create zip
-        buffer3 = io.BytesIO()
-        zip_file = zipfile.ZipFile(buffer3, 'w')
-        zip_file.writestr(filename1, ds_bytes1)
-        zip_file.writestr(filename2, ds_bytes2)
-        zip_file.close()
-        # Return zip
-        response = HttpResponse(buffer3.getvalue())
-        response['Content-Type'] = 'application/x-zip-compressed'
-        response['Content-Disposition'] = 'attachment; filename=album.zip'
-        return response
-        # current_path = os.getcwd()
-        # ds.save_as(current_path + "/IM11.dcm", write_like_original=False)
-        # response = FileResponse(document.file, )
-        # response["Content-Disposition"] = "attachment; filename=" + file_name
-
-        # Get filename from url
-        filename = "temp.dcm"
-        # Create zip
-        buffer2 = io.BytesIO()
-        zip_file = zipfile.ZipFile(buffer2, 'w')
-        zip_file.writestr(filename, strMyfile)
-        zip_file.close()
-        # Return zip
-        response = HttpResponse(buffer2.getvalue())
-        response['Content-Type'] = 'application/x-zip-compressed'
-        response['Content-Disposition'] = 'attachment; filename=album.zip'
-        return response
-        # return Response(data=myfile, status=status.HTTP_200_OK)
