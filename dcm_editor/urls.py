a4933c51f6d2248eab1e83f1c2fbe64b8c22c8a1
dwnusa <dwnusa@gmail.com>
2020-11-14 09:06:01 +0900

    two class as_view included, 'hello', 'index'

--- dcm_editor/urls.py
+++ dcm_editor/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index_as_view.as_view(), name='index_as_view'),
+    path('hello', views.Hello.as_view(), name='hello'),
 ]
--- dcm_editor/views.py
+++ dcm_editor/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Create your views here.
+from django.http import HttpResponse
+# from sntemplate.eval_tf import eval
+import os
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index!")
+
+
+class index_as_view(APIView):
+    def get(self, request, format=None):
+        return Response(data="index world!", status=status.HTTP_200_OK)
+
+
+class Hello(APIView):
+    # def get(self, format=None):
+    def get(self, request, format=None):
+        return Response(data="Hello world!", status=status.HTTP_200_OK)
+
